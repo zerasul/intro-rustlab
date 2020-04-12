b4402af3d35d4036092dc94d837fb39da910c919
zerasul <zerasul@gmail.com>
2020-04-12 17:32:03 +0200

    modificando el enunciado

--- modulos/modulos1.rs
+++ modulos/modulos1.rs
@@ -3,11 +3,15 @@
  * Autor: Victor Suarez <zerasul@gmail.com>
 */
 
-
+pub mod mimodulo{
+    pub fn mifuncion(x:i32)->i32{
+        2*x
+    }
+}
 
 fn main(){
     let x=2;
 
-    println!("El doble de {} es: {}",mimodulo::mifuncion(x));
+    //TODO: Llamar al modulo
 
 }
