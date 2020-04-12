5ee327fe832f817c2087edf33b571264e59423f3
zerasul <zerasul@gmail.com>
2020-04-12 17:29:10 +0200

    solucion ejercicio de los modulos.

--- modulos/modulos1.rs
+++ modulos/modulos1.rs
@@ -3,11 +3,16 @@
  * Autor: Victor Suarez <zerasul@gmail.com>
 */
 
-
+pub mod mimodulo{
+     
+    pub fn mifuncion(x:i32)->i32{
+        2*x
+    }
+}
 
 fn main(){
     let x=2;
 
-    println!("El doble de {} es: {}",mimodulo::mifuncion(x));
+    println!("El doble de {} es: {}",x, mimodulo::mifuncion(x));
 
 }
