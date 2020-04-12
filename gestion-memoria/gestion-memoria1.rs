5beb1d847047c149bec49805ff0f31033fb17efa
zerasul <zerasul@gmail.com>
2020-04-12 11:19:07 +0200

    añadiendo solucion

--- gestion-memoria/gestion-memoria1.rs
+++ gestion-memoria/gestion-memoria1.rs
@@ -3,15 +3,17 @@
  * Autor: Victor Suarez <zerasul@gmail.com>
 */
 
-fn anyadir_uno(v:Vec<i32>,x:i32)->Vec<i32>{
-
-    v.push(x);
-    v
+fn anyadir_uno(v:&Vec<i32>,x:i32)->Vec<i32>{
+    let mut vec1=v.clone();
+    vec1.push(x);
+    vec1
 }
 
 fn main(){
     let v0 = Vec::new();
-    let v1=anyadir_uno(v0, 3);
+    let v1=anyadir_uno(&v0, 3);
 
     println!("El valor de v0 es: {:?}",v0); //para formatear un vector se usa {:?}
+
+    println!("El valor de v1 es: {:?}",v1);
 }
