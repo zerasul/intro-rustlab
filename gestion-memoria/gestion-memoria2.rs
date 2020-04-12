4dbd551680e206175c81dbe8a57b5fa4c4380c16
zerasul <zerasul@gmail.com>
2020-04-12 11:25:06 +0200

    añadiendo ejercicio2 de gestion-memoria

--- gestion-memoria/gestion-memoria2.rs
+++ gestion-memoria/gestion-memoria2.rs
@@ -1,18 +1,20 @@
-/* Ejercicio 8: gestion memoria Rust
+/* Ejercicio 9: gestion memoria Rust
  * Modificar este ejercicio para que compile
  * Autor: Victor Suarez <zerasul@gmail.com>
 */
 
-fn anyadir_uno(v:Vec<i32>,x:i32)->Vec<i32>{
+fn anyadir_uno(v:&Vec<i32>,x:i32)->Vec<i32>{
 
+    let mut v=v.clone();
     v.push(x);
     v
 }
 
 fn main(){
     let v0 = Vec::new();
-    let v1=anyadir_uno(v0, 3);
+    let v1=anyadir_uno(&v0, 3);
 
     println!("El valor de v0 es: {:?}",v0); //para formatear un vector se usa {:?}
+    v1.push(4);
     println!("El valor de v1 es: {:?}",v1);
 }
